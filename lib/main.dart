0e8eb2061736553b5172bdca248e4dce14cb6be6
28Hari <bloggerhari3@gmail.com>
2023-02-21 13:03:26 +0530

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromRGBO(41, 40, 40, 1),
